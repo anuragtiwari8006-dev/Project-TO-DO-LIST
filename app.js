6e1cf35962eef0df9774aa1f142626848f88b2a7
anuragtiwari8006-dev <anuragtiwari8006@gmail.com>
2026-04-01 00:33:20 +0530

    new

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ btn.addEventListener("click", function(){
     let item=document.createElement("li");
     item.innerText=inp.value;
     ul.appendChild(item);
+    
+    
     inp.value=""
     let delbtn=document.createElement("button");
     delbtn.innerText="delete";
     delbtn.classList.add("delete");
     item.appendChild(delbtn);
+    
+    inp.value=""
    
     
     
